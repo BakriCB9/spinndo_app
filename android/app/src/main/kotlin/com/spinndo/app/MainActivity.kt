c457930ace505e5c0ba770ce6a3314b529eacb7b
Fara7-Touma <touma.farah@hotmail.com>
2025-06-02 18:09:11 +0300

    final

--- android/app/src/main/kotlin/com/spinndo/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/spinndo/app/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.spinndo.app
 
+import io.flutter.embedding.android.FlutterActivity
+
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity() {
